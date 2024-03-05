457c463ff7964e5cba1d1dc79dbc1c234a9360eb
nc-katherine-hurst <katherine.hurst@northcoders.com>
2024-03-05 10:08:23 +0000

    moved validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { postSlackMessage } = require("./api");
+const crypto = require("crypto");
 
 const app = express();
 const port = 3000;
@@ -8,14 +9,27 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.post("/zoom-webhook", async (req, res) => {
-  const { payload, event } = req.body;
-  try {
-    await postSlackMessage("testing 1 2 3")
-    res.status(200).send("OK");
-  } catch (error) {
-    res.status(error.response.status).send({
-      msg: "There be a problem",
+  if (req.body.event === "endpoint.url_validation") {
+    const hashForValidate = crypto
+      .createHmac("sha256", ZOOM_WEBHOOK_SECRET_TOKEN)
+      .update(request.body.payload.plainToken)
+      .digest("hex");
+
+    res.status(200);
+    res.json({
+      plainToken: request.body.payload.plainToken,
+      encryptedToken: hashForValidate,
     });
+  } else {
+    const { payload, event } = req.body;
+    try {
+      await postSlackMessage("testing 1 2 3");
+      res.status(200).send("OK");
+    } catch (error) {
+      res.status(error.response.status).send({
+        msg: "There be a problem",
+      });
+    }
   }
 });
 
