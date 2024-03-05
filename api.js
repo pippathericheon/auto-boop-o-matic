c6db8879d3abeaaab2b2a61540337c8e0f6088aa
nc-katherine-hurst <katherine.hurst@northcoders.com>
2024-03-05 10:00:53 +0000

    added crypto func for validation pls work

--- api.js
+++ api.js
@@ -1,7 +1,22 @@
 const axios = require("axios");
+const crypto = require("crypto");
 require("dotenv").config();
 
 const postSlackMessage = (messageContent) => {
+  // Webhook request event type is a challenge-response check
+  if (request.body.event === "endpoint.url_validation") {
+    const hashForValidate = crypto
+      .createHmac("sha256", ZOOM_WEBHOOK_SECRET_TOKEN)
+      .update(request.body.payload.plainToken)
+      .digest("hex");
+
+    response.status(200);
+    response.json({
+      plainToken: request.body.payload.plainToken,
+      encryptedToken: hashForValidate,
+    });
+  }
+
   const blocks = [
     {
       type: "section",
