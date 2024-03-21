856d7ff0adf0fde58df6f4dd3345d98811872ff1
nc-katherine-hurst <katherine.hurst@northcoders.com>
2024-03-21 11:25:15 +0000

    altered message

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { postSlackMessage } = require("./api");
+
+require("dotenv").config();
 const crypto = require("crypto");
 
 const app = express();
@@ -10,20 +12,22 @@ app.use(bodyParser.json());
 
 app.post("/zoom-webhook", async (req, res) => {
   if (req.body.event === "endpoint.url_validation") {
+    console.log("validating....");
     const hashForValidate = crypto
-      .createHmac("sha256", ZOOM_WEBHOOK_SECRET_TOKEN)
-      .update(request.body.payload.plainToken)
+      .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
+      .update(req.body.payload.plainToken)
       .digest("hex");
 
     res.status(200);
-    res.json({
-      plainToken: request.body.payload.plainToken,
+    const responseObj = {
+      plainToken: req.body.payload.plainToken,
       encryptedToken: hashForValidate,
-    });
+    };
+    res.json(responseObj);
   } else {
     const { payload, event } = req.body;
     try {
-      await postSlackMessage("testing 1 2 3");
+      await postSlackMessage("A zoom meeting has ended");
       res.status(200).send("OK");
     } catch (error) {
       res.status(error.response.status).send({
